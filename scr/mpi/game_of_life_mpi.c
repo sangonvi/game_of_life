9744785ac9ded4280d536b83029ac47c880781be
C1325305 Vinicius Goncalves Dos Santos <c1325305@interno.bb.com.br>
2024-09-16 17:07:19 -0300

    updates serial and mpi versions

--- scr/mpi/game_of_life_mpi.c
+++ scr/mpi/game_of_life_mpi.c
@@ -11,14 +11,16 @@
 
 int allocMemory(int ***matrix, int nrows, int ncolumns);
 void allocVectorMemory(int **vector, int ncolumns);
+void getPreviousAndNextRanks(int my_rank, int *previous_rank, int *next_rank, int nprocesses);
 void fieldCreation(int **matrix, int rank, int nrows, int ncolums);
 int isCorner(int i, int j, int nrows, int ncolumns);
 int getCorrespondingCorner(int **current_grid, int i, int j, int nrows, int ncolumns);
 int getCorrespondingValue(int **current_grid, int i, int j, int nrows, int ncolumns, int *top_ghost, int *bottom_ghost);
-int accountNeighboursCells(int **current_grid, int i, int j, int nrows, int ncolumns, int *top_ghost, int *bottom_ghost);
+int countNeighboursCells(int **current_grid, int i, int j, int nrows, int ncolumns, int *top_ghost, int *bottom_ghost);
 void fillTotalGrid(int **completedGrid, int rank, int **gridOfRank, int rows, int columns);
 int cellUpdate(int cell, int count);
 void printTotalGrid(int **totalGrid, int rows, int columns, int timeStep);
+void freeMemory(int **matrix, int nrows);
 
 int main(int argc, char** argv) {
    	int nprocesses;
@@ -56,46 +58,44 @@ int main(int argc, char** argv) {
 		allocMemory(&total_grid, nrows, ncolumns);
 	}
 
+	getPreviousAndNextRanks(my_rank, &previous_rank, &next_rank, nprocesses);
 	fieldCreation(current_grid, my_rank, rows_per_process, ncolumns); 
-	next_rank = my_rank + 1;
-	previous_rank= my_rank - 1;
-
-	if (my_rank == 0){
-		previous_rank +=nprocesses;
-
-	}
-	if (my_rank == (nprocesses-1)){
-		next_rank -= nprocesses;
-	}
-
+	
 	for (int time=1; time <= timesteps; time++){
 		MPI_Send(current_grid[0], ncolumns, MPI_INT, previous_rank, tag, MPI_COMM_WORLD);
 		MPI_Send(current_grid[rows_per_process-1], ncolumns, MPI_INT, next_rank, tag, MPI_COMM_WORLD);
 
+        MPI_Recv(bottom_ghost, ncolumns, MPI_INT, next_rank, tag,MPI_COMM_WORLD, &status);
 		MPI_Recv(top_ghost, ncolumns, MPI_INT, previous_rank, tag,MPI_COMM_WORLD, &status);
-		MPI_Recv(bottom_ghost, ncolumns, MPI_INT, next_rank, tag,MPI_COMM_WORLD, &status);
+	
 
         for (int i = 0; i < rows_per_process; i++) {
 			for (int j = 0; j < ncolumns; j++) {
-				int count = accountNeighboursCells(current_grid, i, j, 
+				int count = countNeighboursCells(current_grid, i, j, 
 													rows_per_process, ncolumns, top_ghost, bottom_ghost);
 				next_grid[i][j] = cellUpdate(current_grid[i][j], count);
 			}
 		}
 
-	    *current_grid = *next_grid;
+        *current_grid = *next_grid;
+
 		if (my_rank == 0){
 			fillTotalGrid(total_grid ,my_rank, next_grid, rows_per_process, ncolumns);
 			for (int rank=1; rank<nprocesses; rank++){
-				MPI_Recv(next_grid, ncolumns*rows_per_process, MPI_INT, rank, tag,MPI_COMM_WORLD, &status);
+				MPI_Recv(&next_grid[0][0], rows_per_process*ncolumns, MPI_INT, rank, tag,MPI_COMM_WORLD, &status);
 				fillTotalGrid(total_grid ,rank, next_grid, rows_per_process, ncolumns);
 			}
 			printTotalGrid(total_grid,nrows,ncolumns,time);
 		}else{
-			MPI_Send(next_grid, ncolumns*rows_per_process, MPI_INT, 0, tag, MPI_COMM_WORLD);	
+			MPI_Send(&next_grid[0][0], rows_per_process*ncolumns, MPI_INT, 0, tag, MPI_COMM_WORLD);	
 		}
         	
     } 
+
+    //freeMemory(next_grid, rows_per_process);
+	//freeMemory(current_grid, rows_per_process);
+	//freeMemory(total_grid, nrows);
+	
 
 	MPI_Finalize();
 
@@ -127,17 +127,44 @@ void allocVectorMemory(int **vector, int ncolums){
 	(*vector) = malloc(ncolums * sizeof(int *));
 }
 
+void freeMemory(int **matrix,int nrows) {
+  for (int i = 0; i < nrows; i++)
+    free(matrix[i]);
+  free(matrix);
+}
+
+void getPreviousAndNextRanks(int my_rank, int *previous_rank, int *next_rank, int nprocesses){
+	*next_rank = my_rank + 1;
+	*previous_rank= my_rank - 1;
+
+	if (my_rank == 0){
+		*previous_rank +=nprocesses;
+
+	}
+	if (my_rank == (nprocesses-1)){
+		*next_rank -= nprocesses;
+	}
+}
 
 void fieldCreation(int **matrix, int rank, int nrows, int ncolumns) {
-   srand(time(NULL));
-   for (int i = 0; i < nrows; i++) {
-    for (int j = 0; j < ncolumns; j++) {
-		if (( (double)rand() / (double)RAND_MAX ) > 0.5) {
-        	matrix[i][j] = 1;
-        }else{
-            matrix[i][j] = 0;
-        }
-    }
+   FILE *arq;
+   arq = freopen("../../presets/randomInput.txt", "r", stdin);
+
+   int pos = rank*ncolumns*nrows;
+   fseek(arq,pos, SEEK_SET);
+
+   for (int i=0; i < nrows; i++) {
+		for (int j = 0; j < ncolumns; j++) {
+			char c;
+			fscanf(arq,"%c ", &c);
+			if (c == '0')
+				matrix[i][j] = 0;
+			
+			if (c == '1')
+				matrix[i][j] = 1;
+				
+		}
+		printf("\n");
    }
 }
 
@@ -177,7 +204,7 @@ int getCorrespondingValue(int **current_grid, int i, int j, int nrows, int ncolu
 	return current_grid[i][j];
 }
 
-int accountNeighboursCells(int **current_grid, int i, int j, int nrows, int ncolumns, int *top_ghost, int *bottom_ghost) {
+int countNeighboursCells(int **current_grid, int i, int j, int nrows, int ncolumns, int *top_ghost, int *bottom_ghost) {
   int count = 0;
   for (int index_i= i-1; index_i <= i+1; index_i++) {
     for (int index_j = j-1; index_j <= j+1; index_j++) {	
