30cf590f5f198a0f6b2d2ef1964f68cd0be36e89
C1325305 Vinicius Goncalves Dos Santos <c1325305@interno.bb.com.br>
2024-09-16 10:44:50 -0300

    add random_input.c script

---
+++ scr/util/random_input.c
@@ -0,0 +1,35 @@
+#include <ncurses.h>
+#include <curses.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <mpi.h>
+#include <time.h>
+
+void createRandomInput(int rows, int columns);
+
+int main(int argc, char** argv){
+    int rows = atoi(argv[1]);
+	  int columns = atoi(argv[2]);
+    createRandomInput(rows, columns);
+    return 0;
+}
+
+void createRandomInput(int rows, int columns){
+  srand(time(NULL));
+  FILE *arq;
+  arq = fopen("../../presets/randomInput.txt", "w");
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) { 
+       if (( (double)rand() / (double)RAND_MAX ) > 0.5) {
+         fputc('1',arq);
+       }else{
+         fputc('0', arq);
+       }
+    }
+  }  
+
+  fclose(arq);
+}
