f5fc4bcc1a554bb3327895da2805e0d8a94faf72
C1325305 Vinicius Goncalves Dos Santos <c1325305@interno.bb.com.br>
2024-09-30 00:08:31 -0300

    test

--- scr/post_processing/post_processing.py
+++ scr/post_processing/post_processing.py
@@ -1,5 +1,20 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv('../report.csv')
 
-df = pd.read_csv('..report.csv')
+mpi = df[df['metodo'] == 'MPI']
+openMP = df[df['metodo'] == 'OPEN_MP']
+serial = df[df['metodo'] == 'SERIAL']
+print(serial)
+plt.plot(mpi['cores'], mpi['tempo_execucao'],'ro-', 
+         openMP['cores'], openMP['tempo_execucao'], 'b^--', 
+         serial['cores'], serial['tempo_execucao'], 'gs')
+plt.legend(['MPI', 'OpenMP', 'Serial'])
+plt.xlabel("Número de cores ")
+plt.ylabel("Tempo de execução(s)")
+plt.title("100 timesteps, Grid 1024x1024")
+plt.savefig("tempos_de_Execucao.png") 
 
-print(df.to_string()) 
+print(df)
+plt.show()
+
